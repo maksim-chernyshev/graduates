5235c3c4e4cff655e496279fa4b9ec2128d011ac
maksim_chernyshev <maksim.tch@gmail.com>
2022-09-06 00:08:31 +0300

    - added reviews slider; - fixed mission slider;

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,48 @@
 const missionSwiper = document.querySelector('.mission__cards')
+const reviewsSwiper = document.querySelector('.reviews__inner')
 
 document.addEventListener('DOMContentLoaded', function () {
-    const swiper = new Swiper(missionSwiper, {
-        grabCursor: true,
-        slidesPerView: 3,
-        freeMode: true,
-        slidesPerGroup: 1,
+    const swiperMission = new Swiper(missionSwiper, {
+        loop: true,
+        slidesPerView: 'auto',
+        autoplay: true,
+        speed: 500,
 
         breakpoints: {
             320: {
+                loop: false,
                 enabled: false,
-                slidesPerView: 1
             },
             768: {
-                enabled: true,
                 spaceBetween: 24,
             },
             1024: {
                 spaceBetween: 32
+            }
+        }
+    })
+
+    const swiperReviews = new Swiper(reviewsSwiper, {
+        loop: true,
+        slidesPerView: 'auto',
+        autoplay: true,
+        speed: 500,
+        navigation: {
+            nextEl: '.reviews__btn--next',
+            prevEl: '.reviews__btn--prev',
+        },
+
+        breakpoints: {
+            320: {
+                spaceBetween: 20,
+            },
+            768: {
+                autoplay: false,
+                spaceBetween: 16,
+            },
+            1024: {
+                autoplay: false,
+                spaceBetween: 32,
             }
         }
     })
