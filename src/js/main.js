6f188f043652c1a32f67256481cf4ea32e0a0b47
maksim_chernyshev <maksim.tch@gmail.com>
2022-09-06 15:19:20 +0300

    - added full form section; - added footer; - added navigation modal for mobile resolution; - added logic for landing;

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,91 @@
-const missionSwiper = document.querySelector('.mission__cards')
-const reviewsSwiper = document.querySelector('.reviews__inner')
-
 document.addEventListener('DOMContentLoaded', function () {
+    const DEFAULT_REF = 'ef45402a410905ec'
+
+    const missionSwiper = document.querySelector('.mission__cards')
+    const reviewsSwiper = document.querySelector('.reviews__inner')
+
+    const headerSection = document.querySelector('[data-section="header"]')
+    const navigationModal = document.querySelector('[data-section="modalNav"]')
+    const openNavigationModalButton = document.querySelector('[data-btn="openNavigation"]')
+    const closeNavigationModalButton = document.querySelector('[data-btn="closeNavigation"]')
+    const logoLinks = document.querySelectorAll('[data-js="logoLink"]')
+    const forms = document.querySelectorAll('form')
+
+    const headerLinks = headerSection.querySelectorAll('[data-anchor]')
+    const modalLinks = navigationModal.querySelectorAll('[data-anchor]')
+
+    const urlParams = new URLSearchParams(window.location.search)
+
+    /* добавляем в ссылку-логотип текущий url со всеми гет-параметрами*/
+    logoLinks?.forEach(function(link) {
+        link.setAttribute('href', `${window.location.toString()}`)
+    })
+
+    /* плавный скролл к блокам на странице (декстоп) */
+    headerLinks?.forEach(function (link) {
+        link.addEventListener('click', function (event) {
+            event.preventDefault();
+            const scrollTarget = document.querySelector(`#${link.dataset.anchor}`)
+            scrollTo(scrollTarget, 'center')
+        })
+    })
+
+    openNavigationModalButton?.addEventListener('click', showModal)
+
+    closeNavigationModalButton?.addEventListener('click', hideModal)
+
+
+    modalLinks?.forEach(function (link) {
+        link.addEventListener('click', function (event) {
+            event.preventDefault();
+
+            const scrollTarget = document.querySelector(`#${link.dataset.anchor}`)
+            hideModal();
+
+            if (scrollTarget.id === 'hero') {
+                document.querySelector('.hero-form__input').focus();
+            }
+
+            scrollTo(scrollTarget, 'start')
+        })
+    })
+
+    forms?.forEach(function(form) {
+        const formAction = form.getAttribute('action')
+        const inputs = form.querySelectorAll('input')
+
+        /* подстановка корректных гет-параметров в экшн формы */
+        if (urlParams.has('ref')) {
+            form.setAttribute('action', `${formAction}?${urlParams.toString()}`)
+        } else if (urlParams.toString()) {
+            form.setAttribute('action', `${formAction}?ref=${DEFAULT_REF}&${urlParams.toString()}`)
+        } else {
+            form.setAttribute('action', `${formAction}?ref=${DEFAULT_REF}`)
+        }
+
+        /* проверка инпутов на заполнение и вывод красной рамки */
+        inputs.forEach(function(input) {
+            input.addEventListener('input', function() {
+                input.checkValidity()
+            })
+
+            input.addEventListener('invalid', function() {
+                if (input.value === '') {
+                    input.classList.add('error')
+
+                    setTimeout(function() {
+                        input.classList.remove('error')
+                    }, 5000)
+                }
+            })
+        })
+    })
+
     const swiperMission = new Swiper(missionSwiper, {
         loop: true,
         slidesPerView: 'auto',
         autoplay: true,
-        speed: 500,
+        speed: 1000,
 
         breakpoints: {
             320: {
@@ -21,12 +100,11 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         }
     })
-
     const swiperReviews = new Swiper(reviewsSwiper, {
         loop: true,
         slidesPerView: 'auto',
         autoplay: true,
-        speed: 500,
+        speed: 1000,
         navigation: {
             nextEl: '.reviews__btn--next',
             prevEl: '.reviews__btn--prev',
@@ -46,5 +124,22 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         }
     })
+
+    function scrollTo(targetElement, position) {
+        targetElement.scrollIntoView( {
+            block: position,
+            behavior: 'smooth'
+        })
+    }
+
+    function showModal() {
+        document.body.classList.add('lock-scroll')
+        navigationModal.classList.remove('hidden');
+    }
+
+    function hideModal() {
+        document.body.classList.remove('lock-scroll')
+        navigationModal.classList.add('hidden');
+    }
 })
 
